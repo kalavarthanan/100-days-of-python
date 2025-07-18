cbc9a616708a6098f2777ec79e5c69f02b4571ca
kalavarthanan <kalavarthanan@gmail.com>
2025-07-19 01:06:31 +0530

    day 01 added

---
+++ python-day-1.py
@@ -0,0 +1,45 @@
+
+print("Day 1 - Python Print Function")
+print("The Function Is Declared Like This:")
+print("print('What to print')")
+
+print("hello world!\nhello world!\nhello world!")
+
+print("hello" +" "+"kala")
+
+
+
+
+print("Day 1 - String Manipulation")
+print("String concandation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+
+
+print("Hello "+input("What is your name?\n") + "!")
+a = input("What is your name? ")
+
+print("Hello " + a +"\n" +"Length of your name is : " , len(a))
+print(len(a))
+
+a = input("a : ")
+b = input("b : ")
+
+t = a
+a = b
+b = t
+
+print("a : "+a)
+print("b : "+b)
+
+
+# Day 01 - python project - 01 -Band name generator
+
+
+print("Welcome to the Band Name Generator!")
+city_name = input("Name the city that you grew up in?\n")
+pet_name = input("Name of your pet?\n")
+
+print("Your Band Name could be : " + city_name + " " + pet_name)
